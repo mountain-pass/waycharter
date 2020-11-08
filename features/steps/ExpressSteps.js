c6b98443b96fb8b0c4fe99f767c7907e43554649
Nick Grealy <nickgrealy@gmail.com>
2020-11-08 21:45:13 +1100

    Added noop handler in tests.

--- features/steps/ExpressSteps.js
+++ features/steps/ExpressSteps.js
@@ -5,6 +5,8 @@ const { expect } = require('chai')
 const RSON = require('relaxed-json')
 const Metadata = require('../../src/express/Metadata')
 
+const NOOP_HANDLER = (req, res) => {}
+
 // setup routes
 
 const parsePath = (path) => (~path.indexOf(',') ? path.split(',') : path)
@@ -12,20 +14,20 @@ const parsePath = (path) => (~path.indexOf(',') ? path.split(',') : path)
 Given('the route {string} {string}', function (method, path) {
   const world = this
 
-  world.app[method](parsePath(path), (req, res) => res.json({ message: 'noop' }))
+  world.app[method](parsePath(path), NOOP_HANDLER)
 })
 
 Given('the route {string} {string} with subroute {string} {string}', function (method, path, submethod, subpath) {
   const world = this
   const router = world.express.Router()
-  router[submethod](parsePath(subpath), (req, res) => res.json({ message: 'noop' }))
+  router[submethod](parsePath(subpath), NOOP_HANDLER)
   world.app[method](parsePath(path), router)
 })
 
 Given('the route {string} {string} with metadata', function (method, path, dataTable) {
   const world = this
   const meta = dataTable.hashes()[0]
-  world.app[method](parsePath(path), new Metadata(meta), (req, res) => res.json({ message: 'noop' }))
+  world.app[method](parsePath(path), new Metadata(meta), NOOP_HANDLER)
 })
 
 // assert metadata
